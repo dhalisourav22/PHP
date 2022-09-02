244a581a031607e8371108e03db371674728b85e
Sourav Dhali <dhalisourav22@gmail.com>
2022-09-02 16:51:58 +0600

    Update main.php

--- Spaceship Operator/main.php
+++ Spaceship Operator/main.php
@@ -1,8 +1,12 @@
 <?php
+
     $x = 7;
     $y = 6;
+
     $result = $x <=> $y;
+
     if($result == -1) echo $x." is the smaller number.";
     elseif($result == 0) echo $x." ane ".$y." both are same.";
     else echo $x." is the bigger number.";
+
 ?>
